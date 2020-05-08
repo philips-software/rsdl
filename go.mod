eaa1ea8f55ffa79f3e00801a10b5676324433d75
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-05-08 17:52:18 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,35 @@
+module rsdl
+
+go 1.14
+
+require (
+	github.com/aws/aws-sdk-go v1.30.9 // indirect
+	github.com/cloudfoundry-community/gautocloud v1.1.5
+	github.com/cloudfoundry-community/go-cfenv v1.18.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/golang/mock v1.4.3 // indirect
+	github.com/golang/protobuf v1.4.0 // indirect
+	github.com/hsdp/gautocloud-connectors v0.0.0-20200418103355-ed48b4f51ea0
+	github.com/inconshreveable/log15 v0.0.0-20200109203555-b30bc20e4fd1 // indirect
+	github.com/kevinburke/go-types v0.0.0-20200309064045-f2d4aea18a7a // indirect
+	github.com/kevinburke/rest v0.0.0-20200329062440-b3f4200b8f35 // indirect
+	github.com/kevinburke/twilio-go v0.0.0-20200203063821-378e630e02da // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/labstack/echo/v4 v4.1.16
+	github.com/labstack/gommon v0.3.0
+	github.com/lib/pq v1.3.0
+	github.com/loafoe/sqltocsv v0.0.0-20200421080913-1d34850504cb
+	github.com/mitchellh/mapstructure v1.2.2 // indirect
+	github.com/pelletier/go-toml v1.7.0 // indirect
+	github.com/sirupsen/logrus v1.5.0 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.6.3
+	github.com/ttacon/libphonenumber v1.1.0 // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
+	golang.org/x/sys v0.0.0-20200413165638-669c56c373c4 // indirect
+	gopkg.in/ini.v1 v1.55.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"crypto/subtle"
+	"fmt"
+	"github.com/cloudfoundry-community/gautocloud"
+	"github.com/cloudfoundry-community/gautocloud/connectors/databases/dbtype"
+	_ "github.com/hsdp/gautocloud-connectors/hsdp"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+	"github.com/lib/pq"
+	"github.com/loafoe/sqltocsv"
+	"github.com/spf13/viper"
+	"net/http"
+	"os"
+	"time"
+)
+
+var GitCommit = "deadbeaf"
+
+func main() {
+	log.Infof("rsdl version: %s", GitCommit)
+
+	// Config
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.SetEnvPrefix("rsdl")
+	viper.SetDefault("password", "")
+	viper.SetDefault("schema", "")
+	viper.SetDefault("gzip", "true")
+	viper.AutomaticEnv()
+	viper.AddConfigPath(".")
+	viper.ReadInConfig()
+
+	password := viper.GetString("password")
+	if password == "" { // Never without a password
+		log.Error("missing password")
+		return
+	}
+	schemaName := viper.GetString("schema")
+	if schemaName == "" {
+		log.Error("missing schema")
+		return
+	}
+	useCompression := viper.GetBool("gzip")
+	usePort := os.Getenv("PORT")
+	if usePort == "" {
+		usePort = "8080"
+	}
+
+	// Database
+	var rs *dbtype.PostgresqlDB
+	err := gautocloud.Inject(&rs)
+	if err != nil {
+		log.Errorf("database error: %v", err)
+		return
+	}
+
+	// Webapp
+	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
+	if useCompression {
+		e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+			Level: 5,
+		}))
+	}
+	e.Use(middleware.BasicAuth(authCheck))
+	e.Use(middleware.Logger())
+	e.GET("/redshift/:tableName/:type", downloader(rs, schemaName))
+	e.GET("/redshift/:schema/:tableName/:type", downloader(rs, schemaName))
+	log.Fatal(e.Start(":" + usePort))
+	return
+}
+
+// authCheck verifies basic auth. Username hardcoded to `redshift`
+func authCheck(username, password string, c echo.Context) (bool, error) {
+	if subtle.ConstantTimeCompare([]byte(username), []byte("redshift")) == 1 &&
+		subtle.ConstantTimeCompare([]byte(password), []byte(password)) == 1 {
+		return true, nil
+	}
+	return false, nil
+}
+
+// downloader queries and streams TAB separated CSV of a pg table
+func downloader(rs *dbtype.PostgresqlDB, schemaName string) echo.HandlerFunc {
+	return func(e echo.Context) error {
+		schema := e.Param("schema")
+		tableName := e.Param("tableName")
+		downloadType := e.Param("type")
+
+		quotedTable := pq.QuoteIdentifier(tableName)
+		quotedSchema := schemaName // Default
+		if schema != "" {
+			quotedSchema = pq.QuoteIdentifier(schema)
+		}
+
+		rows, err := rs.QueryContext(e.Request().Context(),
+			fmt.Sprintf("SELECT * FROM %s.%s", quotedSchema, quotedTable))
+
+		if err != nil {
+			e.String(http.StatusBadRequest, err.Error())
+			return err
+		}
+		defer rows.Close()
+
+		switch downloadType {
+		case "full.csv":
+		default:
+			e.String(http.StatusBadRequest, "unsupported download type")
+			return nil
+		}
+
+		resp := e.Response()
+		header := resp.Header()
+		header.Set(echo.HeaderContentType, echo.MIMEOctetStream)
+		header.Set(echo.HeaderContentDisposition, "attachment; filename="+"full.csv")
+		header.Set("Content-Transfer-Encoding", "binary")
+		header.Set("Expires", "0")
+
+		h, _ := rows.Columns()
+
+		var count = 0
+		csvConverter := sqltocsv.New(rows)
+		csvConverter.Delimiter = '\t'
+		csvConverter.Headers = h
+		csvConverter.SetRowPreProcessor(func(row []string, names []string) (outputRow bool, processedRow []string) {
+			count++
+			return true, row
+		})
+		csvConverter.SetContext(e.Request().Context())
+		done := make(chan bool)
+		log := e.Logger()
+		go func() {
+			for {
+				select {
+				case <-done:
+					log.Infof("done with request: %d rows total", count)
+					return
+				case <-time.After(2 * time.Second):
+					log.Infof("processed %d rows", count)
+				}
+			}
+		}()
+		err = csvConverter.Write(resp.Writer)
+		done <- true
+		return err
+	}
+}
